5ed75dea716283c1f048980a72250fa49513db66
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2020-01-30 06:36:53 +0300

    Profile screen & sign-out.

--- app/src/main/java/com/example/mapsapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mapsapp/ui/profile/ProfileFragment.java
@@ -1,26 +1,60 @@
 package com.example.mapsapp.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.example.mapsapp.LoginActivity;
 import com.example.mapsapp.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
 
-    private ProfileViewModel profileViewModel;
+    GoogleSignInClient mGoogleSignInClient;
 
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        return view ;
+
+        //Sign-in Client
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity().getApplicationContext(), gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getActivity().getApplicationContext());
+
+        //Display Name
+        TextView display_name = view.findViewById(R.id.display_name);
+        display_name.setText(account.getDisplayName());
+
+        //Profile Picture
+        CircleImageView profile_image = view.findViewById(R.id.profile_image);
+        Picasso.get().load(account.getPhotoUrl()).into(profile_image);
+
+        //Handle Logout Button
+        ImageView logout_image = view.findViewById(R.id.logout);
+        logout_image.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mGoogleSignInClient.signOut();
+                Intent myIntent = new Intent(getActivity().getApplicationContext(), LoginActivity.class);
+                myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(myIntent);
+            }
+        });
+
+        return view;
     }
 }
