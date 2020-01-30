742e9a4ce4f68e72de0829856c8bffd928f14420
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2020-01-30 18:36:44 +0300

    Request Permission.

--- app/src/main/java/com/example/mapsapp/ui/map/MapFragment.java
+++ app/src/main/java/com/example/mapsapp/ui/map/MapFragment.java
@@ -1,10 +1,14 @@
 package com.example.mapsapp.ui.map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.mapsapp.R;
@@ -25,6 +29,9 @@ public class MapFragment extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        //Check Permissions
+        checkAndRequestPermission();
 
         //Inıt Google Maps
         mMapView = rootView.findViewById(R.id.map);
@@ -73,10 +80,25 @@ public class MapFragment extends Fragment  {
 
     public void onMapReadyHandler(GoogleMap mMap) {
         googleMap = mMap;
-        googleMap.setMyLocationEnabled(true);
+        //googleMap.setMyLocationEnabled(true);
         LatLng sydney = new LatLng(-34, 151);
         googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
         CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    public void checkAndRequestPermission() {
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, getTargetRequestCode());
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            Toast.makeText(getActivity().getApplicationContext(), "izin verilmeli", Toast.LENGTH_LONG).show();
+            checkAndRequestPermission();
+        }
     }
 }
