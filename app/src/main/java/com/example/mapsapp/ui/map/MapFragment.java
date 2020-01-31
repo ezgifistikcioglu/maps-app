765033cf81f51083cf7da01502f2b121b1f5dbc8
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2020-01-31 16:42:34 +0300

    Distance alert algorithm.

--- app/src/main/java/com/example/mapsapp/ui/map/MapFragment.java
+++ app/src/main/java/com/example/mapsapp/ui/map/MapFragment.java
@@ -3,11 +3,13 @@ package com.example.mapsapp.ui.map;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,7 @@ import android.widget.Toast;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 import com.example.mapsapp.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -33,11 +36,17 @@ public class MapFragment extends Fragment implements LocationListener  {
     MapView mMapView;
     private GoogleMap googleMap;
     Marker marker;
+    SharedPreferences prefs;
     boolean firstPositionUpdate = true;
+    LatLng start_position = null;
+    boolean toast_is_show = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        //Get Shared Preferences
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
         //Init Google Maps
         mMapView = rootView.findViewById(R.id.map);
@@ -130,6 +139,7 @@ public class MapFragment extends Fragment implements LocationListener  {
 
     @Override
     public void onLocationChanged(Location location) {
+        checkDistance(new LatLng(location.getLatitude(), location.getLongitude()));
         marker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
         marker.setVisible(true);
         float zoom_level = (firstPositionUpdate) ? 22 : googleMap.getCameraPosition().zoom;
@@ -146,5 +156,27 @@ public class MapFragment extends Fragment implements LocationListener  {
 
     @Override
     public void onProviderDisabled(String provider) {
+    }
+
+    public void checkDistance(LatLng loc) {
+        if(start_position==null) {
+            start_position = loc;
+        }
+        float[] results = new float[1];
+        Location.distanceBetween(start_position.latitude, start_position.longitude, loc.latitude, loc.longitude, results);
+        float distance = results[0];
+        Log.i("distance", "konum farkı: "+results[0]);
+        Integer alert_distance = prefs.getInt("alert_distance", 5);
+        if(distance > alert_distance) {
+            if(!toast_is_show) {
+                toast_is_show = true;
+                Toast.makeText(getActivity(), "Lütfen boşlangıç konuuna dönün!", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            if(toast_is_show) {
+                toast_is_show = false;
+                Toast.makeText(getActivity(), "Başlangıç konuuna yaklaştınız!", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
