4f10f157dc29fa9b299ac6ae950b51d8287f1e1c
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2020-01-31 12:50:42 +0300

    User's current position.

--- app/src/main/java/com/example/mapsapp/ui/map/MapFragment.java
+++ app/src/main/java/com/example/mapsapp/ui/map/MapFragment.java
@@ -1,7 +1,12 @@
 package com.example.mapsapp.ui.map;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,25 +17,32 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.mapsapp.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment  {
+public class MapFragment extends Fragment implements LocationListener  {
 
     MapView mMapView;
     private GoogleMap googleMap;
+    Marker marker;
+    boolean firstPositionUpdate = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
 
-        //Inıt Google Maps
+        //Init Google Maps
         mMapView = rootView.findViewById(R.id.map);
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume();
-
 
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
@@ -83,7 +95,7 @@ public class MapFragment extends Fragment  {
     public void checkAndRequestPermission() {
         if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
-            requestPermissions(new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, getTargetRequestCode());
+            requestPermissions(new String[] { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION }, getTargetRequestCode());
         }
         else
         {
@@ -98,6 +110,41 @@ public class MapFragment extends Fragment  {
         checkAndRequestPermission();
     }
 
+    @SuppressLint("MissingPermission")
     public void mapActions() {
+        //Get User Account
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getActivity().getApplicationContext());
+
+        //Add Marker
+        MarkerOptions marker_options = new MarkerOptions()
+                .position(new LatLng(10, 10))
+                .title(account.getDisplayName());
+        marker = googleMap.addMarker(marker_options);
+        marker.setVisible(false);
+
+        //Current Location Subscription
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER, 1000, 1, this);
+        Toast.makeText(getActivity().getApplicationContext(), "Konum bilginiz alınıyor!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        marker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+        marker.setVisible(true);
+        float zoom_level = (firstPositionUpdate) ? 22 : googleMap.getCameraPosition().zoom;
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), zoom_level));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
     }
 }
